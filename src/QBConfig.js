2c885bd0ba42bd3355f0cdfe49263aaba34915fb
brandonlroberts@outlook.com <brandonroberts@Brandons-MBP.lan1>
2020-05-02 14:18:18 -0400

    initial commit

---
+++ src/QBConfig.js
@@ -0,0 +1,8 @@
+export default {
+  appId: '82194',
+  authKey: 'eDdPxyPd96fCYmu',
+  authSecret: 'Y4Qy4ZZkqydPbAP',
+  accountKey: 'cHXAoyChMUZh3TzXLcpH',
+  apiEndpoint: '',
+  chatEndpoint: '',
+}
